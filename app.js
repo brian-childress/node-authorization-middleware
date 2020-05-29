e6ee79708aec28f226ceed9cd8eee73087516d6c
Brian Childress <brian@brianchildress.co>
2020-05-29 05:08:37 -0400

    Fixes

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const authorize = require("./auth-stuff");
-const config = require("./config");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/token", (req, res) => {
-  const payload = {
-    name: "Jimmy",
-    scopes: "customer:read"
-  };
-
-  const token = jwt.sign(payload, 'Mysecret');
-  res.send(token);
-});
-
-app.get("/customer", authorize("customer:read"), (req, res) => {
+app.get("/customer", (req, res) => {
   res.send("Customer Information");
 });
 
-const server = app.listen(5000, () => {
-  console.log(`Server is listening on 3000`);
+const server = app.listen(port, () => {
+  console.log(`Server is listening on ${server.address().port}`);
 });
