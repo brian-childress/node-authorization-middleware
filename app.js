1811ada42ad153049e6cb91de57ebfe9128fb72e
Brian Childress <brian@brianchildress.co>
2020-05-29 05:20:43 -0400

    Fixes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.get("/customer", (req, res) => {
   res.send("Customer Information");
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server is listening on ${server.address().port}`);
+app.listen(5000, () => {
+  console.log(`Server is listening...`);
 });
